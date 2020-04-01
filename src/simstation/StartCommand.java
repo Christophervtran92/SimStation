895d9151ffb69a52cedc81a6f2f384fbcb7f428b
Christophervtran92 <61164533+Christophervtran92@users.noreply.github.com>
2020-04-01 09:37:45 -0700

    Update StartCommand.java
    Added in the code for the command but waiting on Simulation class. I called the class Simulation based on the progress sheet but we can always refactor it to SimStation/simstation/etc. if needed.

--- src/simstation/StartCommand.java
+++ src/simstation/StartCommand.java
@@ -3,13 +3,14 @@ package simstation;
 import mvc.Command;
 import mvc.Model;
 
-public class StartCommand extends Command {
-    public StartCommand(Model model) {
-        super(model);
-    }
+public class StartCommand extends Command{
 
-    @Override
-    public void execute() {
+    public StartCommand(Model m) {super(m); }
 
+    public void execute()
+    {
+        Simulation sim = (Simulation)model; //Waiting on Simulation class
+        sim.start();                        //Will make start command following the above
     }
 }
+
