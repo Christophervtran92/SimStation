a44f809a0e9ba61d30e9aa9ac9ab40923d149459
Christophervtran92 <61164533+Christophervtran92@users.noreply.github.com>
2020-04-01 09:30:00 -0700

    Update SimulationFactory.java
    Adding in the commands that are shown on the images from the assignment page. I called it 'Simulation' instead of 'SimStation' based on the progress sheet so if we are going with SimStation we can refactor it and change the makeModel method to return new SimStation instead.

--- src/simstation/SimulationFactory.java
+++ src/simstation/SimulationFactory.java
@@ -4,34 +4,38 @@ import mvc.AppFactory;
 import mvc.Command;
 import mvc.Model;
 
-public class SimulationFactory implements AppFactory {
-    @Override
-    public Model makeModel() {
-        return null;
-    }
+public class SimulationFactory implements AppFactory{
 
-    @Override
+    public Model makeModel() { return new Simulation(); }   //Waiting on simulation class
+
     public String[] getEditCommands() {
-        return new String[0];
+        return new String[]{"Start", "Suspend", "Resume", "Stop", "Stats"};
     }
 
-    @Override
+    //Creating the commands; waiting on the Command classes
     public Command makeEditCommand(Model model, String type) {
+        if(type == "Start")
+            return new StartCommand(model);
+        else if(type == "Suspend")
+            return new SuspendCommand(model);
+        else if(type == "Resume")
+            return new ResumeCommand(model);
+        else if(type == "Stop")
+            return new StopCommand(model);
+        else if(type == "Stats")
+            return new StatsCommand(model);
         return null;
     }
 
-    @Override
-    public String getTitle() {
-        return null;
-    }
+    //Title of the window
+    public String getTitle() { return "Simstation"; }   //Perhaps this should be overwritten for each sim
 
-    @Override
     public String[] getHelp() {
-        return new String[0];
+        return new String[] {"Simstation for multiple simualtions."};   //Overwritten for each sim
     }
 
-    @Override
-    public String about() {
-        return null;
-    }
+    public String about() { return "SimStation\n by Haruna Yamakawa, Christopher Tran, and Gabriel Tenocelotl"; }
 }
+
+
+
