4a522c902ed2902f075a13c093234f8def9608a6
Christophervtran92 <61164533+Christophervtran92@users.noreply.github.com>
2020-04-01 09:42:17 -0700

    Update StopCommand.java
    Added in the code for the command but waiting on Simulation class. I called the class Simulation based on the progress sheet but we can always refactor it to SimStation/simstation/etc. if needed.

--- src/simstation/StopCommand.java
+++ src/simstation/StopCommand.java
@@ -4,12 +4,12 @@ import mvc.Command;
 import mvc.Model;
 
 public class StopCommand extends Command {
-    public StopCommand(Model model) {
-        super(model);
-    }
-
-    @Override
-    public void execute() {
-
+    
+    public StopCommand(Model m) { super(m); }
+    
+    public void execute()
+    {
+        Simulation sim = (Simluation)model; //Waiting on Simulation class
+        sim.stop();                         //Will make stop command following the above
     }
 }
